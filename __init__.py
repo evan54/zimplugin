8830522ef280369a7919420e6862c03945fe244a
evan54 <evan54@gmail.com>
2020-10-29 22:57:37 -0400

    test

--- __init__.py
+++ __init__.py
@@ -1,10 +1,44 @@
+# Copyright 2011 Patricio Paez <pp@pp.com.mx>
+#
+# Plugin to use arithmetic in Zim wiki
+
+from zim.inc.arithmetic import ParserGTK
+
 from zim.plugins import PluginClass
+from zim.actions import action
 
-class MyPlugin(PluginClass):
+from zim.gui.pageview import PageViewExtension
 
-plugin_info = {
-    'name': _('My Plugin'),
-    'description': _('My first plugin')
-    'author': 'Your Name',
-}
+
+class ArithmeticPlugin(PluginClass):
+
+	plugin_info = {
+		'name': _('Test'), # T: plugin name
+		'description': _('''\
+This plugin allows you to embed arithmetic calculations in zim.
+It is based on the arithmetic module from
+http://pp.com.mx/python/arithmetic.
+copy of Arithmetic
+'''), # T: plugin description
+		'author': 'Patricio Paez',
+		'help': 'Plugins:Arithmetic',
+	}
+
+	#~ plugin_preferences = (
+		# key, type, label, default
+	#~ )
+
+
+class ArithmeticPageViewExtension(PageViewExtension):
+
+	@action(_('_Arithmetic'), accelerator='F5') # T: menu item
+	def calculate(self):
+		"""Perform arithmetic operations"""
+
+		# get the buffer
+		buf = self.pageview.textview.get_buffer() # XXX
+
+		# parse and return modified text
+		parser = ParserGTK()
+		parser.parse(buf)
 
